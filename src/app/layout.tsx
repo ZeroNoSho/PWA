70c3d13425412df89b7e322f08f8d57f9d2960ae
ZeroNoSho <inukuro351@gmail.com>
2025-03-14 03:15:29 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Point Of Sale" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
